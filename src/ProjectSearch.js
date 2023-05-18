d9b7cbba80bef7e8da730a893fca828ccad70e39
outs1der <jcsanchez.martinez.2020@gmail.com>
2023-05-18 12:56:52 -0500

    v2.2.6

--- src/ProjectSearch.js
+++ src/ProjectSearch.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import './ProjectSearch.css';
 import searchIcon from './icons/searchIcon.png';
 
 function ProjectSearch() {
+  const [searchValue, setSearchValue] = React.
+    useState('');
+
+    console.log('se buscan los proyectos de ' + searchValue);
+
   return(
     <li>
       <div className='search'>
         <input 
           placeholder="Search Project"
+          value={searchValue}
           onChange={(event) => {
-            console.log(event.target.value)
+            setSearchValue(event.target.value);
         }}/>
 				<button type='submit'><img src={searchIcon}/></button>
       </div>
